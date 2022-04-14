deae9d6aea01817614d4e683e4f81b1d1a493a40
GeorgeHu <dhxxhch@126.com>
2022-04-14 16:45:47 +0800

    完成公用函数的测试

--- 02_dart_library.dart
+++ 02_dart_library.dart
@@ -1,9 +1,9 @@
 // dart:core test
 void dartcore_numbers_collection_string() {
   // 测试dart核心库中数字、集合、字符串的功能
-  print('\n'+'#'*40);
+  print('\n' + '#' * 40);
   print("测试dart核心库中数字、集合、字符串的功能");
-  print('#'*40);
+  print('#' * 40);
 
   assert(int.parse('42') == 42);
   assert(int.parse('0x42') == 66);
@@ -12,10 +12,10 @@ void dartcore_numbers_collection_string() {
   // 和Java类似，类的toString方法可以手动调用，也会在print时自动调用
   assert(42.toString() == '42');
   assert(123.456.toString() == '123.456');
-  
+
   // 此处的Fix是四舍五入的
   assert(123.456.toStringAsFixed(2) == '123.46');
-  
+
   // 科学计数法的互相转换
   assert(123.456.toStringAsPrecision(2) == '1.2e+2');
   assert(double.parse('1.2e+2') == 120.0);
@@ -136,8 +136,86 @@ void the_sets() {
   assert(intersection.contains('xenon'));
 }
 
+void the_maps() {
+  // 直接创建Map
+  var hawaiianBeaches = {
+    'Oahu': ['Waikiki', 'Kailua', 'Waimanalo'],
+    'Big Island': ['Wailea Bay', 'Pololu Beach'],
+    'Kauai': ['Hanalei', 'Poipu']
+  };
+
+  var searchTerms = Map();
+  // 指定键值的类型
+  var nobleGases = Map<int, String>();
+  nobleGases = {54: 'xenon'};
+  assert(nobleGases[54] == 'xenon');
+  assert(nobleGases.containsKey(54));
+  // 按键值删除Map中的键值对
+  nobleGases.remove(54);
+  assert(!nobleGases.containsKey(54));
+  // 获得Map的所有键的Iterator
+  var keys = hawaiianBeaches.keys;
+  assert(keys.length == 3);
+  assert(Set.from(keys).contains('Oahu'));
+  // 获得Map的所有值的Iterator
+  var values = hawaiianBeaches.values;
+  assert(values.length == 3);
+  assert(values.any((element) => element.contains('Kailua')));
+
+  hawaiianBeaches = {
+    'Oahu': ['Waikiki', 'Kailua', 'Waimanalo'],
+    'Big Island': ['Wailea Bay', 'Pololu Beach'],
+    'Kauai': ['Hanalei', 'Poipu']
+  };
+  assert(hawaiianBeaches.containsKey('Oahu'));
+  assert(!hawaiianBeaches.containsKey('Florida'));
+
+  var teamAssignments = <String, String>{};
+  String pickToughestKid() {
+    return "pickToughestKid";
+  }
+  // 若key不存在于该Map中，就加入
+  teamAssignments.putIfAbsent('Catcher', () => pickToughestKid());
+  assert(teamAssignments['Catcher'] != null);
+}
+
+void the_public_methods() {
+  // 一些通用的公共方法测试
+  var coffees = <String>[];
+  var teas = ['green', 'black', 'chamomile', 'earl grey'];
+  assert(coffees.isEmpty);
+  assert(teas.isNotEmpty);
+  teas.forEach((element) => print('I drink $teas'));
+
+  var hawaiianBeaches = {
+    'Oahu': ['Waikiki', 'Kailua', 'Waimanalo'],
+    'Big Island': ['Wailea Bay', 'Pololu Beach'],
+    'Kauai': ['Hanalei', 'Poipu']
+  };
+
+  hawaiianBeaches.forEach((key, value) {
+    print("I want to visit $key and swim at $value");
+  });
+
+  teas = ['green', 'black', 'chamomile', 'earl grey'];
+  // 和JavaScript中的map用法一样
+  var loudTeas = teas.map((tea) => tea.toUpperCase());
+  loudTeas.forEach(print);
+  var loadTeas_str = loudTeas.toList();
+  print(loadTeas_str);
+  // 用匿名函数创建判断是否是“洋甘菊茶”的函数
+  bool isDecaffeinated(String teaName) => teaName == 'chamomile';
+  // 与数据库类似的查询方法
+  var decaffeinatedTeas = teas.where((tea) => isDecaffeinated(tea));
+  assert(teas.any(isDecaffeinated));
+  assert(!teas.every(isDecaffeinated));
+}
+
 int main() {
   dartcore_numbers_collection_string();
   the_lists();
+  the_sets();
+  the_maps();
+  the_public_methods();
   return 0;
 }
